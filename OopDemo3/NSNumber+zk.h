7aab64679c6c5af0a27a2178bc1539787f48e4a4
calmhb <mac@ayolwas-3.local>
2016-03-04 17:59:06 +0800

    类别

---
+++ OopDemo3/NSNumber+zk.h
@@ -0,0 +1,17 @@
+//
+//  NSNumber+zk.h
+//  OopDemo3
+//
+//  Created by Mac on 16/3/4.
+//  Copyright © 2016年 Mac. All rights reserved.
+//
+
+/*定义类别：扩展NSNumber的功能*/
+@interface NSNumber （zk）
+//在类别中定义4个方法
+-(NSNubmer*）add:(double) num2;
+-(NSNumber*) substract:(double) num2;
+-(NSNumber*) multiply:(double) num2;
+-(NSNumber*) divide:(double) num2;
+
+@end
