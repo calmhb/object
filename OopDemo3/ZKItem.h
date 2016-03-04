7ad3e77fd4be74847caef95f8aad41be2d972ffc
calmhb <mac@ayolwas-3.local>
2016-03-04 09:39:54 +0800

    面向对象3

---
+++ OopDemo3/ZKItem.h
@@ -0,0 +1,15 @@
+//
+//  ZKItem.h
+//  OopDemo3
+//
+//  Created by Mac on 16/3/4.
+//  Copyright © 2016年 Mac. All rights reserved.
+//
+
+@interface ZKItem : NSObject
+@property (nonatomic,assign) int price;
+@property (nonatomic,copy)NSString* name;
+
+
+
+@end
